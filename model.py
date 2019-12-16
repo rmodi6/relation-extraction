366d07cbc7cd88368087e85d3843e3ed7baf2dba
Ruchit Modi <modi.ruchit6@gmail.com>
2019-12-15 22:45:08 -0500

    Added code documentation

--- model.py
+++ model.py
@@ -66,47 +66,40 @@ class MyAdvancedModel(models.Model):
 
     def __init__(self, vocab_size: int, embed_dim: int, hidden_size: int = 128, training: bool = False):
         super(MyAdvancedModel, self).__init__()
-        ### TODO(Students) START
 
         self.num_classes = len(ID_TO_CLASS)
 
         self.decoder = layers.Dense(units=self.num_classes)
         self.embeddings = tf.Variable(tf.random.normal((vocab_size, embed_dim)))
 
-        self.bilstm = layers.Bidirectional(layers.LSTM(units=hidden_size, return_sequences=True), merge_mode='concat')
-        self.cnn_layers = []
-        for kernel_size in [2, 3, 4]:
-            cnn = layers.Convolution1D(hidden_size, input_shape=(None, 2 * hidden_size), kernel_size=kernel_size,
+        self.cnn_layers = []  # List to keep all the cnn layers
+        for kernel_size in [2, 3, 4]:  # Iterate over different kernel sizes
+            # Create a cnn layer for each kernel size, dimension of hidden output is hidden_size (number of filters)
+            cnn = layers.Convolution1D(hidden_size, input_shape=(None, embed_dim), kernel_size=kernel_size,
                                      activation='tanh')
-            self.cnn_layers.append(cnn)
-        self.max_pooling = layers.GlobalMaxPooling1D()
-        self.dropout_layer = layers.Dropout(rate=0.5)
-
-        ### TODO(Students) END
+            self.cnn_layers.append(cnn)  # Add the cnn layer to the list
+        self.max_pooling = layers.GlobalMaxPooling1D()  # Max Pooling layer
+        self.dropout_layer = layers.Dropout(rate=0.5)  # Dropout layer
 
     def call(self, inputs, pos_inputs, training):
         word_embed = tf.nn.embedding_lookup(self.embeddings, inputs)
         pos_embed = tf.nn.embedding_lookup(self.embeddings, pos_inputs)
 
-        ### TODO(Students) START
-
+        # Get the different dimensions of the inputs
         batch_size, sequence_length, embed_size = word_embed.shape
+        # Concatenate the word embeddings and pos embeddings
         input_embed = tf.concat([word_embed, pos_embed], axis=-1)
-        mask = tf.cast(inputs != 0, tf.float32)
 
-        input_embed = self.bilstm(input_embed, mask=mask)
+        hidden_outputs = []  # List to keep hidden outputs of each cnn layer
+        for cnn_layer in self.cnn_layers:  # Iterate over all the cnn layers to be applied
+            hidden_output = cnn_layer(input_embed)  # Apply the cnn layer to the input embeddings
+            hidden_output = self.max_pooling(hidden_output)  # Apply max pooling on the output of the cnn
+            hidden_outputs.append(hidden_output)  # Add the hidden output to the list
 
-        hidden_outputs = []
-        for cnn_layer in self.cnn_layers:
-            hidden_output = cnn_layer(input_embed)
-            hidden_output = self.max_pooling(hidden_output)
-            hidden_outputs.append(hidden_output)
-
-        h = tf.concat(hidden_outputs, axis=-1)
+        h = tf.concat(hidden_outputs, axis=-1)  # Concatenate the max pooled outputs of all the cnn layers
         if training:
-            h = self.dropout_layer(h)
+            h = self.dropout_layer(h)  # Apply dropout if it is in training phase
+        # Feed the concatenated hidden outputs to the final output layer [dense layer]
         logits = self.decoder(h)
-
-        ### TODO(Students) END
 
         return {'logits': logits}
